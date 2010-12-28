609dc5200338cfb5d64eaa5c587939582417c53f
sinsy200 <sinsy200@71a26bd7-015c-fde5-33d3-78198a227c0d>
2010-12-28 06:38:18 +0000

    socialCommentItem 적용. 기존 댓글 표시 개선

--- sandbox/modules/socialxe/socialxe.class.php
+++ sandbox/modules/socialxe/socialxe.class.php
@@ -8,7 +8,8 @@
     require_once(_XE_PATH_.'modules/socialxe/provider.me2day.php');
     require_once(_XE_PATH_.'modules/socialxe/provider.facebook.php');
     require_once(_XE_PATH_.'modules/socialxe/provider.yozm.php');
-    
+    require_once(_XE_PATH_.'modules/socialxe/socialcomment.item.php');
+
     class socialxe extends ModuleObject {
 
         var $hostname = 'socialxe.net';
@@ -18,23 +19,23 @@
             // 설정 정보를 받아옴 (module model 객체를 이용)
             $oModuleModel = &getModel('module');
             $config = $oModuleModel->getModuleConfig('socialxe');
-            
+
             if (!$config->server_hostname) $config->server_hostname = $this->hostname;
             if (!$config->server_query) $config->server_query = $this->query;
             if (!$config->use_ssl) $config->use_ssl = 'Y';
             if (!$config->hashtag) $config->hashtag = 'socialxe';
             $this->config = $config;
-            
+
             // 세션 관리자
             $this->session = &socialxeSessionManager::getInstance();
-            
+
             // 서비스 관리 클래스
             $this->providerManager = &socialxeProviderManager::getInstance($this->session);
-            
+
             // 커뮤니케이터
             $this->communicator = &socialxeCommunicator::getInstance($this->session, $this->providerManager, $this->config);
         }
-        
+
         /**
          * @brief 설치시 추가 작업이 필요할시 구현
          **/
@@ -61,7 +62,7 @@
          **/
         function recompileCache() {
         }
-        
+
         function getNotEncodedFullUrl() {
             $num_args = func_num_args();
             $args_list = func_get_args();
--- sandbox/modules/socialxe/socialxe.model.php
+++ sandbox/modules/socialxe/socialxe.model.php
@@ -7,90 +7,96 @@
          **/
         function init() {
         }
-        
+
         // 환경설정
         function getConfig(){
             // 설정 정보를 받아옴 (module model 객체를 이용)
             $oModuleModel = &getModel('module');
             $config = $oModuleModel->getModuleConfig('socialxe');
-            
+
             if (!$config->server_hostname) $config->server_hostname = $this->hostname;
             if (!$config->server_query) $config->server_query = $this->query;
             if (!$config->use_ssl) $config->use_ssl = 'Y';
             if (!$config->hashtag) $config->hashtag = 'socialxe';
-            
+
             return $config;
         }
-        
+
         // 제공 서비스
         function getProviderList(){
             return $this->providerManager->provider_list;
         }
-        
+
         // 로그인한 서비스 목록
         function loggedProviderList(){
             return $this->providerManager->getLoggedProviderList();
         }
-        
+
         // 서비스 로그인 확인
         function isLogged($provider){
             return $this->providerManager->isLogged($provider);
         }
-        
+
         // 대표 계정의 아이디
         function getID(){
             return $this->providerManager->getMasterProviderId();
         }
-        
+
         // 대표 계정의 닉네임
         function getNickName(){
             return $this->providerManager->getMasterProviderNickName();
         }
-        
+
         // 대표 계정의 프로필 이미지
         function getProfileImage(){
             return $this->providerManager->getMasterProviderProfileImage();
         }
-        
+
         // 대표 계정
         function getMasterProvider(){
             return $this->providerManager->getMasterProvider();
         }
-        
+
         // 소셜 서비스 페이지 링크
         function getAuthorLink($provider, $id){
             return $this->providerManager->getAuthorLink($provider, $id);
         }
-        
+
         // 아이디를 받아 소셜 서비스의 리플 형식으로 반환
         function getReplyPrefix($provider, $id, $nick_name){
             return $this->providerManager->getReplyPrefix($provider, $id, $nick_name);
         }
-        
+
         // 자동 로그인 키
         function getAutoLoginKey(){
             return $this->session->getSession('auto_login_key');
         }
-        
+
         // 자동 로그인 키 요청 URL
         function getAutoLoginKeyUrl(){
             return $this->communicator->getAutoLoginKeyUrl();
         }
-        
+
         // 댓글을 가져온다.
         function getCommentList($document_srl, $last_comment_srl = 0, $count = 10) {
             if (!$count) $count = 10;
 
+            $result = new Object();
+
             $args->document_srl = $document_srl;
             $args->list_count = $count;
-            
+
             // 전체 개수
             if (!$last_comment_srl){
                 $output = executeQuery('socialxe.getCommentCount', $args);
-                if (!$output->toBool()) return;
+                if (!$output->toBool()){
+                    $result->add('list', array());
+                    $result->add('total', 0);
+                    return $result;
+                }
                 $total = $output->data->count;
             }
-            
+
             // 정해진 수에 따라 목록을 구해옴
             if ($last_comment_srl){
                 $args->last_comment_srl = $last_comment_srl;
@@ -98,26 +104,54 @@
             $output = executeQueryArray('socialxe.getCommentPageList', $args);
 
             // 쿼리 결과에서 오류가 생기면 그냥 return
-            if(!$output->toBool()) return;
+            if(!$output->toBool()){
+                $result->add('list', array());
+                $result->add('total', 0);
+                return $result;
+            }
 
-            $output->add('total', $total);
-            return $output;
+            $comment_list = $output->data;
+            if (!$comment_list) $comment_list = array();
+            if (!is_array($comment_list)) $comment_list = array($comment_list);
+
+            // 소셜 댓글 아이템을 생성
+            $socialCommentList = array();
+            foreach($comment_list as $comment){
+                if (!$comment->comment_srl) continue;
+                unset($oSocialComment);
+                $oSocialComment = new socialCommentItem();
+                $oSocialComment->setAttribute($comment);
+                if($is_admin) $oSocialComment->setGrant();
+
+                $socialCommentList[$comment->comment_srl] = $oSocialComment;
+            }
+
+            $result->add('list', $socialCommentList);
+            $result->add('total', $total);
+
+            return $result;
         }
-        
+
         // 대댓글을 가져온다.
         function getSubCommentList($parent_srl, $page = 0, $count = 20) {
             if (!$count) $count = 20;
-            
+
+            $result = new Object();
+
             $args->parent_srl = $parent_srl;
-            
+
             // 페이지가 없으면 제일 뒤 페이지를 구한다.
             if (!$page){
                 $output = executeQuery('socialxe.getSubCommentCount', $args);
-                if (!$output->toBool()) return;
+                if (!$output->toBool()){
+                    $result->add('list', array());
+                    $result->add('page_navigation', new PageHandler(0, 0, 0));
+                    return $result;
+                }
                 $comment_count = $output->data->count;
                 $page = (int)( ($comment_count-1) / $count) + 1;
             }
-            
+
             // 정해진 수에 따라 목록을 구해옴
             $args->page = $page;
             $args->list_count = $count;
@@ -126,45 +160,92 @@
             // 쿼리 결과에서 오류가 생기면 그냥 return
             if(!$output->toBool()) $output;
 
+            $comment_list = $output->data;
+            if (!$comment_list) $comment_list = array();
+            if (!is_array($comment_list)) $comment_list = array($comment_list);
+
+            // 소셜 댓글 아이템을 생성
+            $socialCommentList = array();
+            foreach($comment_list as $comment){
+                if (!$comment->comment_srl) continue;
+                unset($oSocialComment);
+                $oSocialComment = new socialCommentItem();
+                $oSocialComment->setAttribute($comment);
+                if($is_admin) $oSocialComment->setGrant();
+
+                $socialCommentList[$comment->comment_srl] = $oSocialComment;
+            }
+
             // 대댓글 개수 세팅을 위해
             // 부모 댓글의 정보를 가져온다.
             unset($args);
             $args->comment_srl = $parent_srl;
             $output2 = executeQuery('socialxe.getSocialxe', $args);
-            
+
             // 대댓글 개수가 다르면 업데이트한다.
             if ($output2->data && $output2->data->sub_comment_count != $output->total_count){
                 $args->sub_comment_count = $output->total_count;
-                executeQueryArray('socialxe.updateSubCommentCount', $args);
+                executeQuery('socialxe.updateSubCommentCount', $args);
             }
-            
-            return $output;
+
+
+            $result->add('list', $socialCommentList);
+            $result->add('page_navigation', $output->page_navigation);
+            return $result;
         }
-        
+
         // 하나의 특정 댓글을 가져온다.
         function getComment($document_srl, $comment_srl) {
             $args->comment_srl = $comment_srl;
             $args->document_srl = $document_srl;
-            
+
+            $result = new Object();
+
             // 전체 개수
             $output = executeQuery('socialxe.getCommentCount', $args);
-            if (!$output->toBool()) return;
+            if (!$output->toBool()){
+                $result->add('list', array());
+                $result->add('total', 0);
+                return $result;
+            }
             $total = $output->data->count;
-            
+
             // 댓글을 가져온다
             $output = executeQueryArray('socialxe.getComment', $args);
 
             // 쿼리 결과에서 오류가 생기면 그냥 return
-            if(!$output->toBool()) $output;
+            if (!$output->toBool()){
+                $result->add('list', array());
+                $result->add('total', 0);
+                return $result;
+            }
 
-            $output->add('total', $total);
-            $output->add('use_comment_srl', true);
-            
+            $comment_list = $output->data;
+            if (!$comment_list) $comment_list = array();
+            if (!is_array($comment_list)) $comment_list = array($comment_list);
+
+            // 소셜 댓글 아이템을 생성
+            $socialCommentList = array();
+            foreach($comment_list as $comment){
+                if (!$comment->comment_srl) continue;
+                unset($oSocialComment);
+                $oSocialComment = new socialCommentItem();
+                $oSocialComment->setAttribute($comment);
+                if($is_admin) $oSocialComment->setGrant();
+
+                $socialCommentList[$comment->comment_srl] = $oSocialComment;
+            }
+
+            $result->add('total', $total);
+            $result->add('use_comment_srl', true);
+
             // 결과가 없으면 그냥 전체 댓글 리스트를 반환한다.
-            if (!$output->data)
+            if (!count($socialCommentList))
                 return $this->getCommentList($document_srl);
-            else
-                return $output;
+            else{
+                $result->add('list', $socialCommentList);
+                return $result;
+            }
         }
     }
 ?>
