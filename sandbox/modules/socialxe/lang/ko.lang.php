3704363bbdbcf1e1ff61a2c80b58ca9411dea9c9
sinsy200 <sinsy200@71a26bd7-015c-fde5-33d3-78198a227c0d>
2011-01-04 08:03:15 +0000

    명칭을 SocialXE로 통일

--- sandbox/modules/socialxe/lang/ko.lang.php
+++ sandbox/modules/socialxe/lang/ko.lang.php
@@ -1,19 +1,19 @@
 <?php
-    $lang->socialxe = '소셜XE';
+    $lang->socialxe = 'SocialXE';
     $lang->client_token = 'Client Token';
-    $lang->about_client_token = '소셜XE 사이트에서 발급받은 Client Token을 입력하세요.';
-    $lang->server_hostname = '소셜XE 서버의 이름';
-    $lang->about_server_hostname = '소셜XE 기능을 요청할 서버의 도메인을 입력해주세요.';
-    $lang->server_query = '소셜XE 서버 경로';
-    $lang->about_server_query = '소셜XE 기능을 요청할 query url을 입력해 주세요.';
+    $lang->about_client_token = 'SocialXE 사이트에서 발급받은 Client Token을 입력하세요.';
+    $lang->server_hostname = 'SocialXE 서버의 이름';
+    $lang->about_server_hostname = 'SocialXE 기능을 요청할 서버의 도메인을 입력해주세요.';
+    $lang->server_query = 'SocialXE 서버 경로';
+    $lang->about_server_query = 'SocialXE 기능을 요청할 query url을 입력해 주세요.';
     $lang->use_ssl = 'SSL 연결';
-    $lang->about_use_ssl = '소셜XE 서버와 통신할 때 보안연결을 사용합니다.';
+    $lang->about_use_ssl = 'SocialXE 서버와 통신할 때 보안연결을 사용합니다.';
     $lang->hashtag = '태그';
     $lang->about_hashtag = '소셜 서비스로 글이 전송될 때 표시될 태그를 설정하세요. (#생략하고 작성)';
     $lang->select_service = '서비스 선택';
 
     $lang->msg_login = '선택하신 서비스로 로그인 요청 중입니다.<br/>잠시만 기다려주세요.';
-    $lang->msg_request_error = '소셜XE 서버에 API 요청이 실패했습니다.';
+    $lang->msg_request_error = 'SocialXE 서버에 API 요청이 실패했습니다.';
     $lang->msg_invalid_provider = '올바른 서비스 제공자가 아닙니다.';
     $lang->msg_invalid_request_client = '인증된 클라이언트가 아닙니다.';
     $lang->msg_invalid_request_domain = '사용이 허용된 도메인이 아닙니다.';
